590976a2553fefeae867dab3ff6dc92f9b9d9490
sherief <sherif.abdelmgeed@alfarisict.com>
2016-08-29 18:28:59 +0200

    registerCTRL

--- app/scripts/controllers/register.js
+++ app/scripts/controllers/register.js
@@ -1,8 +1,15 @@
 'use strict';
 
 angular.module('psJwtApp')
-  .controller('RegisterCtrl', function ($scope) {
+  .controller('RegisterCtrl', function ($scope,$http) {
     $scope.submit = function(){
         console.log('test');
+        $http.post(url, user)
+        .success(function(res){
+            console.log('good!');
+        })
+        .error(function(err){
+            console.log('bad!');
+        })
     }
   });
